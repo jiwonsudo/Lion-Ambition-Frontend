09c2a1bbcca675309999c9c545853191d086770c
yoojin <everywavyday@gmail.com>
2023-07-12 22:01:44 +0900

    Fix : 토글 JS 수정

--- menu-view/script/menu.js
+++ menu-view/script/menu.js
@@ -46,14 +46,6 @@ function delFile(){
 }
 
 // 상품그룹 드롭다운
-window.onload=()=>{
-  document.querySelector('.dropbtn_click').onmouseover = ()=>{
-    dropdown();
-  }
-  document.getElementsByClassName('menu-list').onclick = ()=>{
-    showMenu(value);
-  };
-
 dropdown = () => {
     var v = document.querySelector('.dropdown-content');
     var dropbtn = document.querySelector('.dropbtn')
@@ -61,35 +53,26 @@ dropdown = () => {
   }
 
 showMenu=(value)=>{
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var dropbtn_content = document.querySelector('.dropbtn_content');
-    var dropbtn_click = document.querySelector('.dropbtn_click');
     var dropbtn = document.querySelector('.dropbtn');
+    var i;
 
     dropbtn_content.innerText = value;
     dropbtn_content.style.color = '#101010';
- }
-}
 
-window.onclick= (e)=>{
-  if(!e.target.matches('.dropbtn_click')){
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var dropbtn_content = document.querySelector('.dropbtn_content');
-    var dropbtn_click = document.querySelector('.dropbtn_click');
-    var dropbtn = document.querySelector('.dropbtn');
-
-    var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
-  }
-}
+ }
 
 //토글 상태 변경
 function is_checked() {
   const checkbox = document.getElementById('toggle-checkbox');
+  var carditem = document.getElementById('card-item');
 
   //checked 속성 체크
   const is_checked = checkbox.checked;
