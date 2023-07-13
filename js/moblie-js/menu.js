613ef42f12c127dea83e7784854f30cead7c8bc0
jiwonsudo <wondev112@gmail.com>
2023-07-13 22:19:30 +0900

    Rename: static/moblie-js -> mobile.js

--- js/moblie-js/menu.js
+++ js/moblie-js/menu.js
@@ -1,18 +1,177 @@
-// HTML 요소 상수 선언
-const modalScreen = document.querySelector('.modal-screen');
-const modalScreenOpenBtns = document.querySelectorAll('.menu-detail-button');
+const productContainer = document.getElementById('menu-container');
 const modalScreenCloseBtn = document.querySelector('.modal-close-button');
+const modalScreen = document.querySelector('.modal-screen');
+const categoryContainer = document.querySelector('.category-container ul');
+
+const cartCookie = document.cookie;
+
+// 프로덕트 렌더링 총괄 함수
+function renderProduct(categoryId, productGetResponse) {
+  const filteredProducts = productGetResponse
+  .filter(item=>item.category.id === categoryId)
+  .map(renderProductToDOM)
+  .join('');
+  productContainer.innerHTML = filteredProducts;
+  setModalScreen();
+  setCounter();
+  setConfirmButton();
+}
+
+// 프로덕트 렌더링 컴포넌트 생성 함수
+function renderProductToDOM(itemData) {
+  return `
+    <div class="menu" id="${itemData.id}">
+      <div class="menu-image-container">
+        <button class="menu-detail-button">
+          <img src="./assets/test_image_1.png">
+        </button>
+      </div>
+      <div class="menu-info">
+        <h1 class="menu-titie">${itemData.name}</h1>
+        <h2 class="menu-price">${itemData.price + '원'}</h2>
+      </div>
+      <div class="menu-selection-container">
+        <div class="quantity-selector">
+          <button class="minus-button">-</button>
+          <span id="count${itemData.id}" class="count-indicator">0</span>
+          <button class="plus-button">+</button>
+        </div>
+        <div class="confirm-selector">
+          <button id="${itemData.id}" class="confirm-button">담기</button>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function renderCategory(categoryGetResponse) {
+  const filteredCategories = categoryGetResponse[0].categories
+  .map(renderCategoryToDOM)
+  .join('');
+  categoryContainer.innerHTML = filteredCategories;
+}
+
+function renderCategoryToDOM(categoryData) {
+  return `
+    <li>
+      <a href="javascript: renderProduct(${categoryData.id}, productGetResponse);">
+        ${categoryData.name}
+      </a>
+    </li>
+  `;
+}
+
+// 모달 스크린 설정 함수
+function setModalScreen() {
+  const modalScreenOpenBtns = document.querySelectorAll('.menu-detail-button');
+
+  for (const modalScreenOpenBtn of modalScreenOpenBtns) {
+    modalScreenOpenBtn.addEventListener('click', toggleModalScreen);
+  }
+  modalScreenCloseBtn.addEventListener('click', toggleModalScreen);
+}
 
 // 모달 스크린 토글 함수
 function toggleModalScreen() {
   modalScreen.classList.toggle('show');
 }
 
-// 모달 스크린 버튼 활성화 (addEventListener)
-for (const modalScreenOpenBtn of modalScreenOpenBtns) {
-  modalScreenOpenBtn.addEventListener('click', toggleModalScreen);
-}
-modalScreenCloseBtn.addEventListener('click', toggleModalScreen);
+// 프로덕트별 카운터 설정 함수
+function setCounter() {
+  const minusButtons = document.querySelectorAll('.minus-button');
+  const plusButtons = document.querySelectorAll('.plus-button');
 
-// API 통신 부분 작성 (추후)
-fetch('https://jsonplaceholder.typicode.com/users')
+  for (const minusButton of minusButtons) {
+    minusButton.addEventListener('click', function() {
+      const countIndicator = minusButton.nextElementSibling;
+      const quantity = Number(countIndicator.textContent);
+      if (quantity > 0) {
+        countIndicator.textContent = quantity - 1;
+      }
+    });
+  }
+
+  for (const plusButton of plusButtons) {
+    plusButton.addEventListener('click', function() {
+      const countIndicator = plusButton.previousElementSibling;
+      const quantity = Number(countIndicator.textContent);
+      if (quantity < 1000) {
+        countIndicator.textContent = quantity + 1;
+      }
+    });
+  }
+}
+
+// 프로덕트별 담기 버튼 설정 함수
+function setConfirmButton() {
+  const confirmButtons = document.querySelectorAll('.confirm-button');
+  for (const confirmButton of confirmButtons) {
+    confirmButton.addEventListener('click', function() {
+      const countIndicator = document.getElementById(`count${confirmButton.id}`);
+      const quantity = Number(countIndicator.textContent);
+      if (quantity > 0) {
+        productGetResponse.forEach(item => {
+          if (localStorage > 0) {
+            console.log(localStorage);
+          }
+
+          if (item.id === Number(confirmButton.id)) {
+            const selectedProductId = item.id;
+            const selectedProductInfo =  {
+              name: item.name,
+              price: item.price,
+              quantity: quantity,
+            }
+            localStorage.setItem(selectedProductId, JSON.stringify(selectedProductInfo));
+          }
+        });
+      }
+      countIndicator.textContent = 0;
+    });
+  }
+}
+
+// 테스트 데이터
+
+const productGetResponse = [
+  {
+    "id": 1,
+    "category": {
+      "id": 1,
+      "name": "햄버거"
+    },
+    "name": "야심작 버거",
+    "image_url": "https://.../image.jpg",
+    "price": 8000,
+    "is_soldout": true
+  },
+  {
+    "id": 99,
+    "category": {
+      "id": 3,
+      "name": "음료"
+    },
+    "name": "불멸의 에너지",
+    "image_url": "https://drive.google.com/.../1021b3a4.jpg",
+    "price": 12000,
+    "is_soldout": false
+  },
+]
+
+const categoryGetResponse = [
+  {
+    "categories": [
+      { "id": 1, "name": "버거" },
+      { "id": 2, "name": "사이드" },
+      { "id": 3, "name": "음료" }
+    ]
+  }
+]
+
+// 이벤트리스너 부착
+// Empty
+
+
+// 페이지 렌더 시 실행부
+renderCategory(categoryGetResponse);
+renderProduct(1, productGetResponse);
